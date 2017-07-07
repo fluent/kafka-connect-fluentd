22c8963efc7d390e9b2525e57d5d64cd01468b03
Kenji Okimoto <okimoto@clear-code.com>
2017-07-07 17:34:45 +0900

    Add simple test for FluentdSourceTask

--- src/test/java/org/fluentd/kafka/FluentdSourceTaskTest.java
+++ src/test/java/org/fluentd/kafka/FluentdSourceTaskTest.java
@@ -1,10 +1,42 @@
 package org.fluentd.kafka;
 
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.komamitsu.fluency.Fluency;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class FluentdSourceTaskTest {
-  @Test
-  public void test() {
-    // Congrats on a passing test!
-  }
+    private FluentdSourceTask task;
+    private Fluency fluency;
+
+    @Before
+    public void setUp() throws IOException {
+        task = new FluentdSourceTask();
+        fluency = Fluency.defaultFluency();
+    }
+
+    @After
+    public void tearDown() {
+        task.stop();
+    }
+
+    @Test
+    public void oneRecord() throws InterruptedException, IOException {
+        Map<String, String> config = new HashMap<>();
+        task.start(config);
+        Map<String, Object> record = new HashMap<>();
+        record.put("message", "This is a test message");
+        fluency.emit("test", record);
+        Thread.sleep(1000);
+        List<SourceRecord> sourceRecords = task.poll();
+        assertEquals(1, sourceRecords.size());
+    }
 }
