6de1b391fb4db80b7fa18c720ad7853ba3e1529f
Kenji Okimoto <okimoto@clear-code.com>
2017-07-07 17:34:31 +0900

    Add test for configuration

--- src/test/java/org/fluentd/kafka/FluentdSourceConnectorConfigTest.java
+++ src/test/java/org/fluentd/kafka/FluentdSourceConnectorConfigTest.java
@@ -1,10 +1,33 @@
 package org.fluentd.kafka;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.net.SocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FluentdSourceConnectorConfigTest {
-  @Test
-  public void doc() {
-    System.out.println(FluentdSourceConnectorConfig.conf().toRst());
-  }
+    @Test
+    public void doc() {
+        System.out.println(FluentdSourceConnectorConfig.conf().toRst());
+    }
+
+    @Test
+    public void defaultValues() {
+        Map<String, String> parsedConfig = new HashMap<>();
+        FluentdSourceConnectorConfig conf = new FluentdSourceConnectorConfig(parsedConfig);
+        assertEquals(24224, conf.getFluentdPort());
+        assertEquals("0.0.0.0", conf.getFluentdBind());
+    }
+
+    @Test
+    public void configValues() {
+        Map<String, String> parsedConfig = new HashMap<>();
+        parsedConfig.put(FluentdSourceConnectorConfig.FLUENTD_PORT, "24225");
+        parsedConfig.put(FluentdSourceConnectorConfig.FLUENTD_BIND, "127.0.0.1");
+        FluentdSourceConnectorConfig conf = new FluentdSourceConnectorConfig(parsedConfig);
+        assertEquals(24225, conf.getFluentdPort());
+        assertEquals("127.0.0.1", conf.getFluentdBind());
+    }
 }
