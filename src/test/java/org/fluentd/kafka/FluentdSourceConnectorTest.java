7bf20cb6d5493325623c4c5a57daf8491546c52c
Kenji Okimoto <okimoto@clear-code.com>
2017-08-24 13:58:52 +0900

    Add small test for FluentdSourceConnector
    This is the first step to add test code.

--- src/test/java/org/fluentd/kafka/FluentdSourceConnectorTest.java
+++ src/test/java/org/fluentd/kafka/FluentdSourceConnectorTest.java
@@ -1,10 +1,40 @@
 package org.fluentd.kafka;
 
+import org.apache.kafka.connect.connector.ConnectorContext;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.powermock.api.easymock.PowerMock;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FluentdSourceConnectorTest {
+    private FluentdSourceConnector connector;
+    private ConnectorContext context;
+
+    @Before
+    public void setup() {
+        connector = new FluentdSourceConnector();
+        context = PowerMock.createMock(ConnectorContext.class);
+        connector.initialize(context);
+    }
+
     @Test
     public void test() {
-        // Congrats on a passing test!
+        PowerMock.replayAll();
+        connector.start(buildSourceProperties());
+        List<Map<String, String>> taskConfigs = connector.taskConfigs(1);
+        Assert.assertEquals("24225", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_PORT));
+        Assert.assertEquals("127.0.0.1", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_BIND));
+        PowerMock.verifyAll();
+    }
+
+    private Map<String, String> buildSourceProperties() {
+        final Map<String, String> sourceProperties = new HashMap<>();
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_PORT, "24225");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_BIND, "127.0.0.1");
+        return sourceProperties;
     }
 }
