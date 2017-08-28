b6a44975f753f296ecf5327f4aa83bf6848e8a00
Kenji Okimoto <okimoto@clear-code.com>
2017-08-28 16:56:07 +0900

    Add more assertion for FluentdSourceConnectorTest

--- src/test/java/org/fluentd/kafka/FluentdSourceConnectorTest.java
+++ src/test/java/org/fluentd/kafka/FluentdSourceConnectorTest.java
@@ -29,6 +29,20 @@ public class FluentdSourceConnectorTest {
         Assert.assertEquals(1, taskConfigs.size());
         Assert.assertEquals("24225", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_PORT));
         Assert.assertEquals("127.0.0.1", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_BIND));
+        Assert.assertEquals("100", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_CHUNK_SIZE_LIMIT));
+        Assert.assertEquals("200", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_BACKLOG));
+        Assert.assertEquals("300", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_SEND_BUFFER_SIZE));
+        Assert.assertEquals("400", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_RECEIVE_BUFFER_SIZE));
+        Assert.assertEquals("false", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEEP_ALIVE_ENABLED));
+        Assert.assertEquals("false", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TCP_NO_DELAY_ENABLED));
+        Assert.assertEquals("2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_WORKER_POOL_SIZE));
+        Assert.assertEquals("tls", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TRANSPORT));
+        Assert.assertEquals("TLSv1.1,TLSv1.2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TLS_VERSIONS));
+        Assert.assertEquals("AES,DES,RSA", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TLS_CIPHERS));
+        Assert.assertEquals("/tmp/keystore.jks", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PATH));
+        Assert.assertEquals("password1", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PASSWORD));
+        Assert.assertEquals("password2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEY_PASSWORD));
+        Assert.assertEquals("topic", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_STATIC_TOPIC));
         PowerMock.verifyAll();
     }
 
@@ -40,6 +54,20 @@ public class FluentdSourceConnectorTest {
         Assert.assertEquals(1, taskConfigs.size());
         Assert.assertEquals("24225", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_PORT));
         Assert.assertEquals("127.0.0.1", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_BIND));
+        Assert.assertEquals("100", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_CHUNK_SIZE_LIMIT));
+        Assert.assertEquals("200", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_BACKLOG));
+        Assert.assertEquals("300", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_SEND_BUFFER_SIZE));
+        Assert.assertEquals("400", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_RECEIVE_BUFFER_SIZE));
+        Assert.assertEquals("false", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEEP_ALIVE_ENABLED));
+        Assert.assertEquals("false", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TCP_NO_DELAY_ENABLED));
+        Assert.assertEquals("2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_WORKER_POOL_SIZE));
+        Assert.assertEquals("tls", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TRANSPORT));
+        Assert.assertEquals("TLSv1.1,TLSv1.2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TLS_VERSIONS));
+        Assert.assertEquals("AES,DES,RSA", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_TLS_CIPHERS));
+        Assert.assertEquals("/tmp/keystore.jks", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PATH));
+        Assert.assertEquals("password1", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PASSWORD));
+        Assert.assertEquals("password2", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_KEY_PASSWORD));
+        Assert.assertEquals("topic", taskConfigs.get(0).get(FluentdSourceConnectorConfig.FLUENTD_STATIC_TOPIC));
         PowerMock.verifyAll();
     }
 
@@ -47,6 +75,20 @@ public class FluentdSourceConnectorTest {
         final Map<String, String> sourceProperties = new HashMap<>();
         sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_PORT, "24225");
         sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_BIND, "127.0.0.1");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_CHUNK_SIZE_LIMIT, "100");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_BACKLOG, "200");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_SEND_BUFFER_SIZE, "300");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_RECEIVE_BUFFER_SIZE, "400");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_KEEP_ALIVE_ENABLED, "false");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_TCP_NO_DELAY_ENABLED, "false");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_WORKER_POOL_SIZE, "2");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_TRANSPORT, "tls");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_TLS_VERSIONS, "TLSv1.1,TLSv1.2");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_TLS_CIPHERS, "AES,DES,RSA");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PATH, "/tmp/keystore.jks");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_KEYSTORE_PASSWORD, "password1");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_KEY_PASSWORD, "password2");
+        sourceProperties.put(FluentdSourceConnectorConfig.FLUENTD_STATIC_TOPIC, "topic");
         return sourceProperties;
     }
 }
