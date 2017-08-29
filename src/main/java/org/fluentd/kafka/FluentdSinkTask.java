4eb57e842f14b747842f50d5b366cba5cbecf748
Kenji Okimoto <okimoto@clear-code.com>
2017-08-29 16:07:21 +0900

    Add missing final identifier to logger

--- src/main/java/org/fluentd/kafka/FluentdSinkTask.java
+++ src/main/java/org/fluentd/kafka/FluentdSinkTask.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 import java.util.Map;
 
 public class FluentdSinkTask extends SinkTask {
-    private static Logger log = LoggerFactory.getLogger(FluentdSinkTask.class);
+    private static final Logger log = LoggerFactory.getLogger(FluentdSinkTask.class);
     private Fluency fluency;
     private SinkRecordConverter converter;
 
